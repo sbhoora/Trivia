59f25ea890281c1d13e25cad209df68c778dad40
sbhoora <shubhibhoora@gmail.com>
2023-09-24 16:35:11 -0400

    can select answer and go to next question

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,65 +2,82 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 function Question() {
-    var amount, category, difficulty, type;
-    amount = "2";
-    category = "&category=" + "9";
-    difficulty = "&difficulty=" + "easy";
-    type = "&type=" + "multiple";
+    const amount = "4";
+    const category = "&category=" + "9";
+    const difficulty = "&difficulty=" + "easy";
+    const type = "&type=" + "multiple";
 
-    const [API, setAPI] = useState("https://opentdb.com/api.php?amount=" + amount + category + difficulty + type);
+    //const [API, setAPI] = useState("https://opentdb.com/api.php?amount=" + amount + category + difficulty + type);
 
     //question number
     const [qNum, setQNum] = useState(0);
 
-    const [qList,setQList] = useState([]);
+    const [qList, setQList] = useState([]);
+
+    const [question, setQuestion] = useState();
+    const [options, setOptions] = useState([]);
+    const [answer, setAnswer] = useState();
 
     const btnClick = () => {
         setQNum(qNum + 1);
+        console.log(qNum);
     }
 
+    
     useEffect(() => {
-        setQList([])
-        fetch(API)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); //GOAL 1
+        if(qList[qNum] != undefined) {
+        console.log('qList has been updated:', qList.length);
+        
+        const { question, answer, options } = qList[qNum];
+        setQuestion(question);
+        setOptions(options);
+        setAnswer(answer);
+        }
+    }, [qNum, qList]); // Add qList to the dependency array
+  
+    
 
-                for(let i = 0; i<data.results.length; i++){
-
-                    const { question, incorrect_answers, correct_answer } = data.results[i];
+    function IDEK(amount, category, difficulty, type) {
+        useEffect(() => {
+            fetch("https://opentdb.com/api.php?amount=" + amount + category + difficulty + type)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data); //GOAL 1
                     
-                    let options =  (
-                    type == "&type=multiple" ? (incorrect_answers.concat(correct_answer).sort(() => Math.random() - 0.5)) :
-                        ["True", "False"]
-                    );
+                    const temp = [];
 
-                    function Question(question,answer,options){
+                    function Question(question, answer, options) {
                         this.question = question;
                         this.answer = answer;
                         this.options = options
                     }
-                    const q = new Question(question, correct_answer, options);
-                    
-                    qList.push(q);
-                    
-                };
-                
-                setQList(qList);
 
-            });
-            
-    }, []); //should only rerender when ALL questions are answered
+                    for (let i = 0; i < data.results.length; i++) {
 
-    console.log("Pass")
+                        const { question, incorrect_answers, correct_answer } = data.results[i];
+
+                        let options = (
+                            type == "&type=multiple" ? (incorrect_answers.concat(correct_answer).sort(() => Math.random() - 0.5)) :
+                                ["True", "False"]
+                        );
+
+                        const q = new Question(question, correct_answer, options);
+
+                        temp.push(q);
+
+                    };
+
+                    setQList(temp);
+                 
+                       
+                });
+        }, []);
+    } 
     
+    IDEK(amount, category, difficulty, type);
 
-    //const { question, answer, options } = qList[qNum];
-   
-    
-
-    return ( null
-        /*
+    return (  
+        
         <>
             <h1>{question}</h1>
             <div>
@@ -72,7 +89,7 @@ function Question() {
                 ))}
             </div>
         </>
-        */
+        
     )
 }
 
