08b07048d1c40c00a8aa17b6f4bd21dc9b44acc8
sbhoora <shubhibhoora@gmail.com>
2023-09-25 04:20:48 -0400

    done css styling for congrats

--- src/components/Congrats.js
+++ src/components/Congrats.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
+import sanrio from '../images/sanrio.png';
 import './Congrats.css';
 
 
@@ -13,7 +14,9 @@ function Congrats() {
         <div className='congrats'>
             <Confetti width={width} height={height}/>
             <h1>✨ CONGRATS YOU HAVE COMPLETED THE QUIZ ✨</h1>
-            <button onClick={() => navigate('/')}>Do Another One!</button>
+            <div id="sanrioImg"><img src={sanrio} alt="sanrioPic" /></div>
+            
+            <button id="newQuizBtn" onClick={() => navigate('/')}>Do Another One!</button>
         </div>        
     )
 }
